72c741d06694e07d7f1c97ea372d5dd39bbaf86c
plugkevo <munenekevin366@gmail.com>
2023-05-18 13:53:21 +0300

    kotlin

--- src/main/kotlin/console.kt
+++ src/main/kotlin/console.kt
@@ -4,8 +4,21 @@ fun main(args:Array<String>){
     //calling the function
     printHello()
 
-    greetings(fname = "Jane")
+    greetings("Jane")
     drive(fspeed = "slow")
+    student(reg = 1234, sname = "kevin")
+    println(double(50))
+    val result =sum(5, 8 )
+    println(result)
+
+ //result for total
+    val total  =add(5, 9)
+
+    // result for area
+    println(area(5 , 6 ))
+
+
+
 }
 // function 2
 fun printHello(){
@@ -21,4 +34,25 @@ fun greetings(fname:String){
 fun drive(fspeed: String){
     println("Going $fspeed")
 
+}
+fun student(reg: Int , sname: String){
+    println("student reg number is $reg and name is $sname")
+
+}
+
+fun double(x:Int): Int{
+ return x * 2
+
+}
+
+//function that adds two numbers
+fun sum(num1:Int, num2:Int):Int{
+     return (num1 + num2)
+}
+fun add(no1:Int, no2:Int)= no1 +no2
+
+// area
+
+fun area(length: Int, width:Int): Int{
+    return (length * width)
 }
